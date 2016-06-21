8c73eea5560786791d1db14a32d41ddc9658e518
guillermodeandajauregui <guillermodeandajauregui@gmail.com>
2016-06-21 15:53:12 -0500

    using density from base R.

--- mi_distribution_plots.R
+++ mi_distribution_plots.R
@@ -10,3 +10,22 @@ hist_sanos <- hist(sanos_sif$V2, breaks = 200, plot = FALSE)
 ##plots histogramas
 plot(x = hist_sanos$mids, y = log(hist_sanos$count), type = "l")
 lines(x = hist_enfermos$mids, y = log(hist_enfermos$count), type = "l", col = "red")
+
+#density plots
+density_enfermos<-density(enfermos_sif$V2)
+density_sanos<-density(sanos_sif$V2)
+
+##plot density
+plot(x = density_enfermos, type = "l", col = "red")
+lines(x = density_sanos, type = "l", col = "blue")
+
+plot(x = density_sanos, type = "l", col = "green")
+#compare densities?
+ks.test(x = density_enfermos$y, y = density_sanos$y)
+
+# #try with KernSmooth
+# bk_enfermos<-bkde(enfermos_sif$V2)
+# bk_sanos<-bkde(sanos_sif$V2)
+# plot(bk_enfermos)
+# plot(bk_sanos)
+# ks.test(x = bk_enfermos$y, y = bk_sanos$y)
