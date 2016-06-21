61ac8b34f5ea38256d497cdd085446765e54a0da
gdj <gdj@ENDOR.inmegen.gob.mx>
2016-06-21 14:32:08 -0500

    rearranged

--- mi_distribution_plots.R
+++ mi_distribution_plots.R
@@ -1,7 +1,12 @@
 require("data.table")
 require("parallel")
 enfermos_sif <-fread(input = "/mnt/e/jespinal/sif/Enfermos1_SinDuplicados.sif", data.table = FALSE)
-head(enfermos_sif)
+sanos_sif <-fread(input = "/mnt/e/jespinal/sif/Sanos1_SinDuplicados.sif", data.table = FALSE)
+
+#histogramas
 hist_enfermos <- hist(enfermos_sif$V2, breaks = 200, plot = FALSE)
-plot(x = hist_enfermos$mids, y = log(hist_enfermos$count), pch = ".")
-lines(x = hist_enfermos$mids, y = log(hist_enfermos$count), type = "l")
+hist_sanos <- hist(sanos_sif$V2, breaks = 200, plot = FALSE)
+
+##plots histogramas
+plot(x = hist_sanos$mids, y = log(hist_sanos$count), type = "l")
+lines(x = hist_enfermos$mids, y = log(hist_enfermos$count), type = "l", col = "red")
