6369443e26dc784f8d4c8138cfa26c984bf302c2
Joshua Haase <hahj87@gmail.com>
2016-06-28 15:51:39 -0500

    Refactor and split using a closure like function.

--- filter_intra_inter_chromosome.R
+++ filter_intra_inter_chromosome.R
@@ -7,49 +7,83 @@
 # - plot the distribution of MI in intra chromosomic interactions and inter chromosomic interactions.
 
 library("biomaRt")
+library("data.table")
+library("argparse")
 
-# TODO:
-# - should we use a network as input for an interactive script?
-# - should we receive a name for the graph as an input?
+# this function gets chromosome name for given gene
+gene.chrom<-function(gene_name, gene_dict){
+	stopifnot(is.character(gene_name))
+	# get the symbol name in the dictionary
+	chrom = gene_dict$chromosome_name[gene_dict$hgnc_symbol == gene_name]
+	if(length(chrom) < 1) { return(NA) }
+	else { return(chrom) }
+}
 
-# FIXME: we did not loaded "genes" in this script,
-#        this script should run by it's own.
+gene.chrom.dict <- function(sif) {
+	# get all the genes in the sif file
+	genes<-unique(c(unique(sif$V1), unique(sif$V3)))
 
-#get genes in networks
-genes<-unique(
-	c(unique(sanos_sif$V1),
-	unique(sanos_sif$V3),
-	unique(enfermos_sif$V1),
-	unique(enfermos_sif$V3))
+	# find the genes on biomart
+	mart=useMart(
+	  biomart="ENSEMBL_MART_ENSEMBL",
+	  dataset="hsapiens_gene_ensembl",
+	  host="www.ensembl.org"
+	)
+	genes_in_chrom <- getBM(
+	  attributes = c("hgnc_symbol", "chromosome_name"),
+	  filters = "hgnc_symbol",
+	  values = genes,
+	  mart = mart
+	)
+
+	# filter only valid gene/chromosome pairs
+	valid_chroms = c(1:23, "X", "Y")
+	genes_in_valid_chroms <- genes_in_chrom[genes_in_chrom$chromosome_name%in%valid_chroms,]
+
+	return(genes_in_valid_chroms)
+}
+
+# takes sif. Returns TRUE for each row that has intrachromosomic interaction
+# FALSE if interchromosomic interaction
+index.intra.chromosomic <- function(sif){
+	dict = gene.chrom.dict(sif)
+
+	# This function takes an index and finds whether both genes are in the same chromosome.
+	# check whether any of the genes are in some chrom
+	in.same.chrom <- function (A, B) {
+		chrom_a = gene.chrom(A, dict)
+		chrom_b = gene.chrom(B, dict)
+
+		# TODO: Can we have an N/A or other error condition?
+		if (is.na(chrom_a) || is.na(chrom_b)) {
+			return(NA)
+		}
+
+		if (chrom_a == chrom_b) {
+			return(TRUE)
+		} else {
+			return(FALSE)
+		}
+	}
+
+	a = sif[,1]
+	b = sif[,3]
+	mapply(FUN = in.same.chrom, A = a, B = b, USE.NAMES = FALSE)
+}
+
+
+# MAIN
+
+parser <- ArgumentParser(description = "Split a sif file in intra/inter chromosomic interactions.")
+parser$add_argument(
+	"file",
+	metavar = 'file',
+	type = 'character',
+	help = "The SIF file to process."
 )
+args <- parser$parse_args()
 
-mart=useMart(
-  biomart="ENSEMBL_MART_ENSEMBL",
-  dataset="hsapiens_gene_ensembl",
-  host="www.ensembl.org"
-)
-
-
-# Make a list of genes in chromosome For each chromosome.
-#
-# This should be done outside of the in_same_chrom checker.
-#
-#  - If we built and saved the entire list from biomaRt,
-#    we could use it as input and save bandwidth and processing.
-#
-#  - It is important however to be able to update the list at will.
-
-
-genes_in_chrom <- getBM(
-  attributes = c("hgnc_symbol", "chromosome_name"),
-  filters = "hgnc_symbol",
-  values = genes,
-  mart = mart
-)
-
-valid_chroms = c(1:23, "X", "Y")
-
-genes_in_valid_chroms <- genes_in_chrom[genes_in_chrom$chromosome_name%in%valid_chroms,]
+sif <- fread(input = args$file, data.table = FALSE)
 
 #offline mode dictionary
 #write.table(genes_in_valid_chroms,
@@ -60,53 +94,25 @@ genes_in_valid_chroms <- genes_in_chrom[genes_in_chrom$chromosome_name%in%valid_
 
 # not_in_chrom<-genes[!(genes%in%genes_in_valid_chroms)]
 
-# TODO: Add a function to make an index of intra chromosome relationships.
+index_intra <- index.intra.chromosomic(sif)
+intra_sif <- sif[index_intra,]
+inter_sif <- sif[!index_intra,]
 
-# We could then map the network interactions through the "in_same_chrom" function
-# to get the vector of intra chromosomic conections.
-# Inter chromosomic connections are the negation on that vector.
+intra_fname = gsub(".(txt|sif)", "_intra.sif", basename(args$file))
+inter_fname = gsub(".(txt|sif)", "_inter.sif", basename(args$file))
 
-# this function gets chromosome name for given gene
-gene.chrom<-function(gene_name){
-	stopifnot(is.character(gene_name))
-	# get the symbol name in the dictionary
-	chrom = genes_in_valid_chroms$chromosome_name[genes_in_valid_chroms$hgnc_symbol == gene_name]
-  if(length(chrom) < 1){
-    return(NA)
-  }
-  else
-	return(chrom)
-}
+write.table(
+	intra_sif,
+	file = intra_fname,
+        quote = FALSE,
+        sep = "\t",
+        row.names = FALSE
+)
 
-
-# This function takes an index and finds whether both genes are in the same chromosome.
-# check whether any of the genes are in some chrom
-in.same.chrom <- function (A, B) {
-	chrom_a = gene.chrom(A)
-	chrom_b = gene.chrom(B)
-
-	# TODO: Can we have an N/A or other error condition?
-	if (is.na(chrom_a) || is.na(chrom_b)) {
-		return(NA)
-	}
-
-	if (chrom_a == chrom_b) {
-		return(TRUE)
-	} else {
-		return(FALSE)
-	}
-}
-
-# takes sif. Returns TRUE for each row that has intrachromosomic interaction
-# FALSE if interchromosomic interaction
-is.intra.chromosomic <- function(sif){
-  a = sif[,1]
-  b = sif[,3]
-  mapply(FUN = in.same.chrom, A = a, B = b, USE.NAMES = FALSE)
-}
-
-# Then we can pass this network through the mi_distribution_plots to get intra and extra.
-#
-# ¿Save the intra and extra networks?
-# ¿Save the intra_chromosomic_index?
-# ¿Repeat Ourselves (not DRY)?
+write.table(
+	inter_sif,
+	file = inter_fname,
+        quote = FALSE,
+        sep = "\t",
+        row.names = FALSE
+)
