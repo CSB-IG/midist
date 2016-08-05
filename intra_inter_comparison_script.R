bd2b05593b49c27b1a5eca594bd392ef7d09b618
guillermodeandajauregui@gmail.com <guillermodeandajauregui@gmail.com>
2016-08-05 16:39:08 -0500

    added plotting whole MI distribution

--- intra_inter_comparison_script.R
+++ intra_inter_comparison_script.R
@@ -55,6 +55,10 @@ inter_test <- subset(sif, is.inter.chromosomic(index))
 intra_pdf<-pdf.sif(sif = intra_test)
 inter_pdf<-pdf.sif(sif = inter_test)
 
+#probability density functions for unseparated MI distribution
+whole_pdf<-pdf.sif(sif = sif) 
+
+
 #move to output directory
 setwd(args$output)
 
@@ -62,10 +66,17 @@ setwd(args$output)
 prefix_fname = basename(args$file)
 
 # Plotting
+
+
 if(args$plots == TRUE){
-plot_density.wo(intra_pdf, inter_pdf, file = paste0(prefix_fname, "_plot.pdf")) #first one red, second blue
-plot_density_log.wo(intra_pdf, inter_pdf, file = paste0(prefix_fname, "_log_plot.pdf")) #first one red, second blue
-plot_density_zoom.wo(intra_pdf, inter_pdf, zoom = 10, file = paste0(prefix_fname,"_zoom_plot.pdf")) #first one red, second blue
+  #plotting whole pdf for MI distribution
+  plot_density.wo(sif, file = paste0("whole_", prefix_fname, "_plot.pdf")) #red 
+  plot_density_log.wo(sif, file = paste0("whole_", prefix_fname, "_log_plot.pdf")) #red 
+  plot_density_zoom.wo(sif, zoom = 10, file = paste0("whole_", prefix_fname,"_zoom_plot.pdf")) #red 
+  #plotting intra and inter pdfs
+  plot_density.wo(intra_pdf, inter_pdf, file = paste0(prefix_fname, "_plot.pdf")) #first one red, second blue
+  plot_density_log.wo(intra_pdf, inter_pdf, file = paste0(prefix_fname, "_log_plot.pdf")) #first one red, second blue
+  plot_density_zoom.wo(intra_pdf, inter_pdf, zoom = 10, file = paste0(prefix_fname,"_zoom_plot.pdf")) #first one red, second blue
 }
 
 #compare via U_test
