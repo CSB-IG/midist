dabfe86482be85c509af4c3426246b2e562386ac
Joshua Haase <jihaase@inmegen.gob.mx>
2016-09-19 13:32:51 -0500

    Split gene.chrom.dict from biomart lookup.

--- chrom.lookup/R/indexing.R
+++ chrom.lookup/R/indexing.R
@@ -1,6 +1,25 @@
 # this function generates a dictionary of gene, chromosome
 # for lookup using gene.chrom
-gene.chrom.dict <- function(genes) {
+#
+# genes_in_chrom: data frame containing fields
+#       hgnc_symbol, chromosome_name
+#
+gene.chrom.dict <- function(genes_in_chrom) {
+
+	# filter only valid gene/chromosome pairs
+	valid_chroms = c(1:23, "X", "Y")
+	genes_in_valid_chroms <- genes_in_chrom[genes_in_chrom$chromosome_name%in%valid_chroms,]
+
+	# convert to dictionary
+	gene_dict = hash::hash(genes_in_valid_chroms$hgnc_symbol, genes_in_valid_chroms$chromosome_name)
+	return(gene_dict)
+}
+
+# Get chromosome info for given genes from bioMart.
+#
+# genes: vector of gene names (string).
+#
+chrom.info.biomart <- function(genes) {
 	# find the genes on biomart
 	mart = biomaRt::useMart(
 		biomart="ENSEMBL_MART_ENSEMBL",
@@ -13,14 +32,7 @@ gene.chrom.dict <- function(genes) {
 		values = genes,
 		mart = mart
 	)
-
-	# filter only valid gene/chromosome pairs
-	valid_chroms = c(1:23, "X", "Y")
-	genes_in_valid_chroms <- genes_in_chrom[genes_in_chrom$chromosome_name%in%valid_chroms,]
-
-	# convert to dictionary
-	gene_dict = hash::hash(genes_in_valid_chroms$hgnc_symbol, genes_in_valid_chroms$chromosome_name)
-	return(gene_dict)
+	return(genes_in_chrom)
 }
 
 # this function gets chromosome name for given gene
@@ -32,7 +44,7 @@ gene.chrom <- function(gene_name, gene_dict) {
 # Takes a list of gene names and a dict containing gene, chromosome.
 #
 # Returns a vector containing a chromosome name for each gene name on genes.
-index.chromosome <- function(genes, chrom_dict = gene.chrom.dict(genes)) {
+index.chromosome <- function(genes, chrom_dict = gene.chrom.dict(chrom.info.biomart(genes))) {
 	chrom_index <- parallel::mcmapply(
 		FUN=gene.chrom,
 		genes,
