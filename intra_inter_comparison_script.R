85d8f5db8fee45b9b6fdbdb64d8b88ab42089109
Joshua Haase <jihaase@inmegen.gob.mx>
2016-10-19 16:08:18 -0500

    Nullmodel fix.
    The nullmodel generation shuffled columns in index instead of shuffling values.

--- intra_inter_comparison_script.R
+++ intra_inter_comparison_script.R
@@ -37,55 +37,96 @@ parser$add_argument(
 
 args <- parser$parse_args()
 
+prefix = basename(args$file)
+
 index <- load.index(args$index)
 sif <- fread(args$file, data.table = FALSE)
 
-#clean NAs
+# clean NAs
 k <- known.chrom(index)
 sif <- subset(sif, k)
 index <- subset(index, k)
 rm(k)
 
-# intra and inter subgraphs
-
-intra_test <- subset(sif, is.intra.chromosomic(index))
-inter_test <- subset(sif, is.inter.chromosomic(index))
-
-#probability density functions
-intra_pdf<-pdf.sif(sif = intra_test)
-inter_pdf<-pdf.sif(sif = inter_test)
-
-#probability density functions for unseparated MI distribution
-whole_pdf<-pdf.sif(sif = sif)
-
-
-#move to output directory
+# move to output directory
 setwd(args$output)
 
-#output prefix
-prefix_fname = basename(args$file)
-
 # Plotting
-
-
 if(args$plots == TRUE){
-  #plotting whole pdf for MI distribution
-  plot_density.wo(whole_pdf, file = paste0("whole_", prefix_fname, "_plot.pdf")) #red
-  plot_density_log.wo(whole_pdf, file = paste0("whole_", prefix_fname, "_log_plot.pdf")) #red
-  plot_density_zoom.wo(whole_pdf, zoom = 10, file = paste0("whole_", prefix_fname,"_zoom_plot.pdf")) #red
-  #plotting intra and inter pdfs
-  plot_density.wo(intra_pdf, inter_pdf, file = paste0(prefix_fname, "_plot.pdf")) #first one red, second blue
-  plot_density_log.wo(intra_pdf, inter_pdf, file = paste0(prefix_fname, "_log_plot.pdf")) #first one red, second blue
-  plot_density_zoom.wo(intra_pdf, inter_pdf, zoom = 10, file = paste0(prefix_fname,"_zoom_plot.pdf")) #first one red, second blue
+	#probability density functions for unseparated MI distribution
+	intra_pdf <- sif.density(subset(sif, is.intra.chromosomic(index)))
+	inter_pdf <- sif.density(subset(sif, is.inter.chromosomic(index)))
+	whole_pdf <- sif.density(sif)
+
+	#plotting whole pdf for MI distribution
+	plot_density.wo(whole_pdf, file = paste0("whole_", prefix, "_plot.pdf")) #red
+	plot_density_log.wo(whole_pdf, file = paste0("whole_", prefix, "_log_plot.pdf")) #red
+	plot_density_zoom.wo(whole_pdf, zoom = 10, file = paste0("whole_", prefix,"_zoom_plot.pdf")) #red
+	#plotting intra and inter pdfs
+	plot_density.wo(intra_pdf, inter_pdf, file = paste0(prefix, "_plot.pdf")) #first one red, second blue
+	plot_density_log.wo(intra_pdf, inter_pdf, file = paste0(prefix, "_log_plot.pdf")) #first one red, second blue
+	plot_density_zoom.wo(intra_pdf, inter_pdf, zoom = 10, file = paste0(prefix,"_zoom_plot.pdf")) #first one red, second blue
 }
 
-#compare via U_test
+# compare distributions using wilcox test
+test.intra.inter <- function(sif, index) {
+	# Probability Density Functions (PDF)
+	intra_pdf <- sif.density(subset(sif, is.intra.chromosomic(index)))
+	inter_pdf <- sif.density(subset(sif, is.inter.chromosomic(index)))
 
-prueba_U<-U_test(pdf1 = intra_pdf, pdf2 = inter_pdf)
-#write out
-u_out <- unlist(prueba_U)
-write.csv(u_out, file = paste0(prefix_fname, ".stats"))
+	result <- wilcox.test(
+		x = intra_pdf$y,
+		y = inter_pdf$y,
+		alternative = "two.sided",
+		paired = FALSE,
+		exact = FALSE
+	)
+	return(result)
+}
 
-#null model
-null_model <- shuffle_repeat(sif, index, n = 100)
-write.csv(null_model, file = paste0(prefix_fname, ".nullmodel"))
+# output filenames
+result_fname <- paste0(prefix, ".stats")
+null_model_fname <- paste0(prefix, ".nullmodel")
+
+result <- test.intra.inter(sif, index)
+
+results_header <- function(wilcox_test_result) {
+	value_names = names(wilcox_test_result)
+	return(c(
+		paste(
+			"# ",
+			wilcox_test_result$method,
+			wilcox_test_result$alternative,
+			wilcox_test_result$data.name,
+			sep = " "
+		),
+		paste(
+			value_names[1],
+			value_names[3],
+			value_names[2],
+			sep = "\t"
+		)
+	))
+}
+
+results_body <-function(wilcox_test_result) {
+	return(paste(
+		result$statistic,
+		result$p.value,
+		result$parameter,
+		sep = "\t"
+	))
+}
+
+write(results_header(result), file = result_fname)
+write(results_body(result), file = result_fname, append = TRUE)
+
+## Null model
+s <- index
+write(results_header(result), file = null_model_fname) # reusing older header because it's just the same
+for (i in 1:100) {
+	s$V1 <- sample(s$V1)
+	s$V2 <- sample(s$V2)
+	result = test.intra.inter(sif, s)
+	write(results_body(result), file = null_model_fname, append = TRUE)
+}
